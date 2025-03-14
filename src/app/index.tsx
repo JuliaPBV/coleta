6ba216df763f8634d5964f096378a8ac793831d4
Julia Pereira <vital.pereira.julia@gmail.com>
2025-03-14 13:29:12 -0300

    iniciando o projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../styles/global.css";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center">
+      <Text className="text-red-600">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
