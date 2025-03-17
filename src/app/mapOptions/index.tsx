e7dfb4fb6127a3db58c673de322d97fbabbc8014
Julia Pereira <vital.pereira.julia@gmail.com>
2025-03-17 18:22:57 -0300

    finalizado o maoOptions

--- src/app/mapOptions/index.tsx
+++ src/app/mapOptions/index.tsx
@@ -1,7 +1,20 @@
-import { View, Pressable, Text, Linking } from "react-native";
+import {
+  View,
+  Pressable,
+  Text,
+  Linking,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import { CaretLeft, X } from "phosphor-react-native";
+import { useRouter } from "expo-router";
+
+const mapsLogo = require("../../assets/logoMaps.png");
+const wazeLogo = require("../../assets/wazeLogo.png");
 
 export default function MapOptions() {
+  const router = useRouter();
+
   return (
     <View>
       <View className="bg-#FFFFFF w-full flex flex-row items-center justify-between p-4 pb-3 border-b border-[#F3F3F3] mb-4">
@@ -20,7 +33,35 @@ export default function MapOptions() {
       </View>
 
       <View className="m-3">
-        <Text className="text-[#4A6FA1] font-bold">Navegar com</Text>
+        <Text className="text-[#4A6FA1] font-bold pb-5">Navegar com</Text>
+
+        <TouchableOpacity
+          onPress={() =>
+            Linking.openURL(
+              "https://www.google.com/maps/search/?api=1&query=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
+            )
+          }
+          className="bg-[#C0D2E9] rounded-full p-4  flex flex-row items-center justify-between"
+        >
+          <Text className="text-[#4A6FA1] font-bold">Google Maps</Text>
+          <Image
+            source={mapsLogo}
+            style={{ width: 30, height: 30 }}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => Linking.openURL("https://www.google.com/maps")}
+          className="bg-[#C0D2E9] rounded-full p-4 mt-4 flex flex-row items-center justify-between"
+        >
+          <Text className="text-[#4A6FA1] font-bold">Waze</Text>
+          <Image
+            source={wazeLogo}
+            style={{ width: 30, height: 30 }}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
