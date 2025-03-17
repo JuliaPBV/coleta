f7fb2d9259e313cfd749f97fa3ac93902cdd40ab
Julia Pereira <vital.pereira.julia@gmail.com>
2025-03-17 14:07:37 -0300

    finalização informations

--- src/components/informations/index.tsx
+++ src/components/informations/index.tsx
@@ -3,20 +3,18 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 export function Informations() {
   return (
     <View className="bg-#FFFFFF w-full p-4">
-      <View className="mb-4">
-        <Text className="text-[#868E96] font-bold ">Responsável</Text>
+      <View className="mb-6">
+        <Text className="text-[#868E96] font-bold pb-1">Responsável</Text>
         <View className="flex flex-row items-center justify-between">
-          <Text className="text-[#4A4A4A] font-bold pb-6">
-            Nome do Responsável
-          </Text>
+          <Text className="text-[#4A4A4A] font-bold ">Nome do Responsável</Text>
           <TouchableOpacity style={styles.circle}>
             <Text className="text-[#2D3177] font-semibold ">ML</Text>
           </TouchableOpacity>
         </View>
       </View>
 
-      <View className="mb-4">
-        <Text className="text-[#868E96] font-bold ">CEP</Text>
+      <View className="mb-6">
+        <Text className="text-[#868E96] font-bold pb-1">CEP</Text>
         <View className="flex flex-row items-center justify-between">
           <Text className="text-[#4A4A4A] font-bold">123456-789</Text>
           <TouchableOpacity style={styles.mapButton}>
@@ -25,26 +23,24 @@ export function Informations() {
         </View>
       </View>
 
-      <View className="mb-4">
-        <Text className="text-[#868E96] font-bold ">Seller</Text>
-        <Text className="text-[#4A4A4A] font-bold pb-6">
+      <View className="mb-6">
+        <Text className="text-[#868E96] font-bold pb-1">Seller</Text>
+        <Text className="text-[#4A4A4A] font-bold">
           Loja de Exemplo Ltda.-789
         </Text>
       </View>
 
-      <View className="mb-4">
-        <Text className="text-[#868E96] font-bold">Horário da Coleta</Text>
-        <Text className="text-[#4A4A4A] font-bold pb-6">09:15 </Text>
+      <View className="mb-6">
+        <Text className="text-[#868E96] font-bold pb-1">Horário da Coleta</Text>
+        <Text className="text-[#4A4A4A] font-bold ">09:15 </Text>
       </View>
 
-      <View className="mb-4">
+      <View className="mb-6">
         <Text className="text-[#868E96] font-bold pb-1">
           Quantidade de Pacotes
         </Text>
         <View className="flex flex-row items-center justify-between">
-          <Text className="text-[#4A4A4A] font-bold pb-6">
-            4 Pacotes - 4,2kg
-          </Text>
+          <Text className="text-[#4A4A4A] font-bold">4 Pacotes - 4,2kg</Text>
           <TouchableOpacity style={styles.viewItemsButton}>
             <Text className="text-white font-bold">Ver itens</Text>
           </TouchableOpacity>
@@ -55,7 +51,7 @@ export function Informations() {
         <Text className="text-[#868E96] font-bold pb-1">
           Endereço da Coleta
         </Text>
-        <Text className="text-[#4A4A4A] font-bold pb-6">
+        <Text className="text-[#4A4A4A] font-bold ">
           Avenida das Estrelas, 78, Bairro Aurora, Cidade Sol - Estado Lunar,
           Complemento, CEP 987654-321
         </Text>
